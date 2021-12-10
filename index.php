02df1a10ea66399bb5fc29b587d2ea2b12f2cf89
Habib30666 <91126413+Habib30666@users.noreply.github.com>
2021-12-10 12:38:44 +0000

    Adding CSS

--- index.php
+++ index.php
@@ -8,11 +8,12 @@ error_reporting(0);
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="style.css">
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php require '/layout/style.php'; ?>
+    <?php 
+    require '/layout/style.php'; ?>
 
 
     <style>
