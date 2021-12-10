a5cdfda59e0989bf55766da93d6f5a510959d169
Habib30666 <91126413+Habib30666@users.noreply.github.com>
2021-12-10 12:41:50 +0000

    Committing files

--- index.php
+++ index.php
@@ -12,8 +12,7 @@ error_reporting(0);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php 
-    require ('/layout/style.php'); ?>
+    <?php include ('/layout/style.php'); ?>
 
 
     <style>
