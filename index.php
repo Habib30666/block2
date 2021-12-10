d928def27029e6cf8c21f9c5240b931606488ca3
Habib30666 <91126413+Habib30666@users.noreply.github.com>
2021-12-10 12:36:57 +0000

    Commenting out

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ error_reporting(0);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <?php include('/layout/style.php'); ?> -->
+    <?php require('/layout/style.php'); ?>
 
 
     <style>
