25920614e06bcc46ff7da5c4b01d637fc111d8be
Habib30666 <91126413+Habib30666@users.noreply.github.com>
2021-12-10 12:35:00 +0000

    CSS Changes

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ error_reporting(0);
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="/css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
