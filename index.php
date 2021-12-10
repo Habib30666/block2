b21ee7933ad90ad33194c1d6f542e09a49cb0b69
Habib30666 <91126413+Habib30666@users.noreply.github.com>
2021-12-10 13:34:48 +0000

    Committing files

--- index.php
+++ index.php
@@ -1,62 +1,128 @@
+<?php
+session_start();  // creates a session or resumes the current one based on a session identifier passed via a GET or POST request
 
+include('class/config.php');
+class signInUp extends database
+{
+    protected $link;
+    public function signUpFunction()
+    {
+        $sql = "SELECT * from astronaut";
+        $res = mysqli_query($this->link, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            return $res;
+        } else {
+            return false;
+        }
+      
+    }
+}
+$obj = new signInUp;
+$objSignUp = $obj->signUpFunction();
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php include('/layout/style.php'); ?>
-
-
+    <?php include('layout/style.php'); ?>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
     <style>
-    .navbar-brand {  /* changes navbar width size for the navbar for the homepage*/
-        width: 10% !important;
+    body {
+        font-family: 'Lato', sans-serif;
+
     }
 
-    .bg_color {   /* changes navbar colour on the homepage*/ 
+    table.dataTable {
+        border-collapse: collapse !important;
+    }
+      /* changes size of navbar on "view astronaur page" */
+    .navbar-brand {
+        width: 7%;
+    }
+      /* change sfont on the "view astronaut page"*/
+    .bg_color {
         background-color: #fff !important;
-    }
-
-    body {     /* changes font type on the homepage (index) */
-        font-family: 'Raleway', sans-serif;
-    }
-
-  
-
-    section {
-        padding: 60px 0;
-    }
-
-   
+    }                   /* changes the navbar colour on the "view astronaut page" */
     </style>
-
 
 </head>
 
 <body class="bg-light">
-    <?php include('/layout/navbar.php'); ?>
+    <?php include('layout/navbar.php'); ?>
+
+    <section>
+        <div class="container bg-white pr-4 pl-4  log_section pb-5">
+
+            <div class="">
+
+
+                <!-- <form action="" method="post"> -->
+                <div class="">
+                    <h4 class="font-weight-bold pt-5 text-center">View Astronaut</h4>
+                    <table id="example" class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Number Mission</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if ($objSignUp) { ?>
+                            <?php while ($row = mysqli_fetch_assoc($objSignUp)) { ?> <!-- while loop will always execute the block of code once, it will then check the condition, and repeat the loop while the specified condition is true -->
+                            <tr>
+                                <th><?php echo $row['name'] ?></th>
+                                <th><?php echo $row['no_missions']; ?></th>
+
+                            </tr>
+                            <?php } ?>
+                            <?php } ?>
+                        </tbody>
 
 
 
+                    </table>
+                </div>
+                <!-- </form> -->
+            </div>
+
+        </div>
+
+    </section>
 
 
-    <div class="container"> <!-- dislays text on the home page -->
-        <h1 class="text-center mt-3">Home Page</h1>
-    </div>
+    <?php include('layout/footer.php'); ?>
+ 
+    <?php include('layout/script.php') ?>         <!-- The HTML <script> src Attribute is used to specify the URL of external JavaScript file. -->
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
+    <script src="js/datepicker.js"></script>
+    <script>
+    $('[data-toggle="datepicker"]').datepicker({
+        autoClose: true,
+        viewStart: 2,
+        format: 'dd/mm/yyyy',    // allows users to input the date of birth on the "create missions" page
 
-    <a href="createAstronaut.php">create Astrounaut</a>
-	<a href="createMission.php">create Mission</a>      <!-- creates links on the homepage to navigate to other pages-->
-    <a href="createtargets.php">create targets</a>
-	<a href="viewAstronaut.php">View Astronauts</a>
-	<a href="viewMission.php">View Missions</a>
+    });
+    // Add the following code if you want the name of the file appear on select
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+    $(document).ready(function() {
+        $('#example').DataTable({
+            responsive: false,
+            dom: 'Bfrtip',
 
 
-    <?php include('/layout/script.php') ?>
-
-
+        });
+    });
+    </script>
 </body>
 
 </html>
